d8cccb266410cbfc0ec279e9690be1f13e4134c9
Hisan Ibrahim <hisan@creatnlrn.com>
2020-02-28 12:04:36 +0530

    added state in context (useState) for storing data, able to randomly add post and showing it on flatlist

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
-const blogPosts = [
-  {
-    title: "Post #1"
-  },
-  { title: "Post #2" }
-];
-
 export const BlogProvider = ({ children }) => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #${blogPosts.length + 1}` }
+    ]);
+  };
   return (
-    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
   );
 };
 
