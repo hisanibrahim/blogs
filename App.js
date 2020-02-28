fe259d95aa78bd33a2ea69a39998390416cee68b
Hisan Ibrahim <hisan@creatnlrn.com>
2020-02-28 11:17:46 +0530

    initial react navigation setup

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScreen";
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
   },
-});
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Hello, You are in index screen.</Text>
+    </View>
+  );
+};
+
+const localStyles = StyleSheet.create({});
+
+export default IndexScreen;
