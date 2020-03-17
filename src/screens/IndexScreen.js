2612f8f4b8b1cdca525b9437339fc7825637ab71
Hisan Ibrahim <hisan@creatnlrn.com>
2020-03-17 20:04:00 +0530

    automatic context creation using a function, wrote some fancy code to pass dispatch

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,16 +1,16 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 
-import BlogContext from "../context/BlogContext";
+import { Context as BlogContext } from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(BlogContext);
   return (
     <View>
       <Text>Hello, You are in index screen.</Text>
       <Button title="Add blog post" onPress={addBlogPost}></Button>
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
